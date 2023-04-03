4485132a6e3e76d990faec63fe0c65c45c2e5149
Prachi24599 <prachi24599@gmail.com>
2023-04-03 13:26:12 +0530

    input and btn class added

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 "use strict";
+const add = document.querySelector(".addition");
+const sub = document.querySelector(".substraction");
+const mult = document.querySelector(".multiplication");
+const div = document.querySelector(".division");
+const buttons = document.querySelectorAll(".btn");
+const displayField = document.querySelector(".input");
+
+const one = document.querySelector(".one");
+// one.addEventListener("click", function () {
+//   //add it to input
+//   displayField.value = one.textContent;
+// });
+
+buttons.forEach((btn) => {
+  btn.addEventListener("click", function () {
+    //add it to input
+    displayField.value = btn.textContent;
+  });
+});
