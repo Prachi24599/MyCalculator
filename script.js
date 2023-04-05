72279fbf83f090137bda778efcc324d287b8e3d0
Prachi24599 <prachi24599@gmail.com>
2023-04-05 22:01:42 +0530

    structure modified

--- script.js
+++ script.js
@@ -1,26 +1,9 @@
 "use strict";
-const add = document.querySelector(".addition");
-const sub = document.querySelector(".substraction");
-const mult = document.querySelector(".multiplication");
-const div = document.querySelector(".division");
+let expression = "";
 const buttons = document.querySelectorAll(".btn");
-const displayField = document.querySelector(".input");
-
-const one = document.querySelector(".one");
-// one.addEventListener("click", function () {
-//   //add it to input
-//   displayField.value = one.textContent;
-// });
 
 buttons.forEach((btn) => {
-  btn.addEventListener("click", function () {
+  btn.addEventListener("click", function (e) {
     //add it to input
-    displayField.value = btn.textContent;
   });
-});
-
-//clear button
-document.querySelector(".clear").addEventListener("click", function () {
-  //clear the input field
-  displayField.value = "";
 });
